18fd0f086be107e6010c928bed78aab4d15043a7
kyungminkim1 <kyungminkimnz@gmail.com>
2022-11-08 11:45:00 +1300

    Complete operate()

--- javascript.js
+++ javascript.js
@@ -18,13 +18,29 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate() {
+function operate(operator, number1, number2) {
     // create result variable
+    let result;
     // check operator using switch statement
+    switch (operator) {
         // if '+', call add(number1, number2)
+        case '+':
+            result = add(number1, number2);
+            break;
         // if '-', call subtract(number1, number2)
+        case '-':
+            result = subtract(number1, number2);
+            break;
         // if 'x', call multiply(number1, number2)
+        case 'x':
+            result = multiply(number1, number2);
+            break;
         // if '÷', call divide(number1, number2)
+        case '÷':
+            result = divide(number1, number2);
+            break;
         // else print error message
+    }   
     // return result
+    return result;
 }
