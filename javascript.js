b3f1a1125fbaafaacb850f276c9139686e0f58a7
kyungminkim1 <kyungminkimnz@gmail.com>
2022-11-08 11:21:10 +1300

    Create operate() pseudocode

--- javascript.js
+++ javascript.js
@@ -17,3 +17,14 @@ function divide(a, b) {
     // return result of a / b
     return a / b;
 }
+
+function operate() {
+    // create result variable
+    // check operator using switch statement
+        // if '+', call add(number1, number2)
+        // if '-', call subtract(number1, number2)
+        // if 'x', call multiply(number1, number2)
+        // if '÷', call divide(number1, number2)
+        // else print error message
+    // return result
+}
